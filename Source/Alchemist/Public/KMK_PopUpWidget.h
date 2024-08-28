0a726563b5fa35200718ca82b760771a8302a54f
ryi01 <kkjmo3@naver.com>
2024-08-28 16:46:30 +0900

    위잿 추가

---
+++ Source/Alchemist/Public/KMK_PopUpWidget.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KMK_PopUpWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALCHEMIST_API UKMK_PopUpWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* PopUpText;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* YesButt;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* NoButt;
+
+	UFUNCTION()
+	void SetPopUpText(FText text);
+	UFUNCTION()
+	void OnClickedYesButt();
+	UFUNCTION()
+	void OnClickedNobutt();
+};
